23114129fe44388a835f9b5dcd91319c3957f766
shijianka <shijianka@foxmail.com>
2022-03-03 20:33:59 +0800

    完善新增用户唯一功能

--- src/main/java/cn/shijianka/reggie/common/GlobalExceptionHandler.java
+++ src/main/java/cn/shijianka/reggie/common/GlobalExceptionHandler.java
@@ -16,6 +16,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
     public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
         log.error(ex.getMessage());
+        if(ex.getMessage().contains("Duplicate entry")){
+            String[] split=ex.getMessage().split(" ");
+            String msg=split[2]+"已存在";
+            return R.error(msg);
+        }
         return R.error("失败了");
     }
 
