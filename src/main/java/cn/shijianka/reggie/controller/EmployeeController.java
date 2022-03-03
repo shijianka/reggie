709cb355567b30493027ff49412833aa53070d1b
shijianka <shijianka@foxmail.com>
2022-03-03 20:15:23 +0800

    新增员工

--- src/main/java/cn/shijianka/reggie/controller/EmployeeController.java
+++ src/main/java/cn/shijianka/reggie/controller/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -53,5 +54,21 @@ public class EmployeeController {
         request.getSession().removeAttribute("employee");
         return R.success("退出成功");
 
+    }
+    //新增员工
+    @PostMapping
+    public R<String> save(HttpServletRequest request, @RequestBody Employee employee){
+        log.info("新增员工，员工信息：{}",employee.toString());
+        //设置初始密码123456，并且md5加密
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //获得当前用户的id
+        Long empId =(Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+        employeeService.save(employee);
+        return R.success("新增员工成功");
     }
 }
