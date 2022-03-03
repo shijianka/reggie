40219d926d1c7c42e02f45c89f62a3352f7e47d1
shijianka <shijianka@foxmail.com>
2022-03-03 20:25:21 +0800

    完善了新增用户名唯一的功能

---
+++ src/main/java/cn/shijianka/reggie/common/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package cn.shijianka.reggie.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@Slf4j
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+@ResponseBody
+public class GlobalExceptionHandler {
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        log.error(ex.getMessage());
+        return R.error("失败了");
+    }
+
+}
