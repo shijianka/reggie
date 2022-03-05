4b29c0e74abe0563aa2353154d61d8b4776c509f
shijianka <shijianka@foxmail.com>
2022-03-05 20:02:47 +0800

    123

---
+++ src/main/java/cn/shijianka/reggie/service/DishService.java
@@ -0,0 +1,9 @@
+package cn.shijianka.reggie.service;
+
+import cn.shijianka.reggie.entity.Dish;
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface DishService extends IService<Dish> {
+}
---
+++ src/main/java/cn/shijianka/reggie/service/impl/DishServiceImpl.java
@@ -0,0 +1,9 @@
+package cn.shijianka.reggie.service.impl;
+
+import cn.shijianka.reggie.entity.Dish;
+import cn.shijianka.reggie.mapper.DishMapper;
+import cn.shijianka.reggie.service.DishService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements DishService {
+}
