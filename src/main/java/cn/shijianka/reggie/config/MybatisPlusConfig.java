6143d037e756588985303f2880e0f4146b379934
shijianka <shijianka@foxmail.com>
2022-03-05 13:46:49 +0800

    分页拦截器需要加Bean注解交给IOC

--- src/main/java/cn/shijianka/reggie/config/MybatisPlusConfig.java
+++ src/main/java/cn/shijianka/reggie/config/MybatisPlusConfig.java
@@ -2,6 +2,7 @@ package cn.shijianka.reggie.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -9,7 +10,8 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class MybatisPlusConfig {
-    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
         mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
         return mybatisPlusInterceptor;
