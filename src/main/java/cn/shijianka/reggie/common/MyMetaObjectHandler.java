5485e2caa57b75aae83bbf303c14247f1cd328d4
shijianka <shijianka@foxmail.com>
2022-03-05 13:44:50 +0800

    公共字段自动填充初步实现

--- src/main/java/cn/shijianka/reggie/common/MyMetaObjectHandler.java
+++ src/main/java/cn/shijianka/reggie/common/MyMetaObjectHandler.java
@@ -12,17 +12,19 @@ import java.time.LocalDateTime;
 public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
-    log.info("start insert fill insert...");
-    log.info(metaObject.toString());
-    metaObject.setValue("creatTime", LocalDateTime.now());
-    metaObject.setValue("creatUser",new Long(1));
+        log.info("新增操作公共字段自动填充...");
+        log.info(metaObject.toString());
+        metaObject.setValue("createTime", LocalDateTime.now());
+        metaObject.setValue("createUser", new Long(123));
+        metaObject.setValue("updateTime", LocalDateTime.now());
+        metaObject.setValue("updateUser", new Long(123));
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        log.info("start insert fill update...");
+        log.info("更改操作公共字段自动填充...");
         log.info(metaObject.toString());
         metaObject.setValue("updateTime", LocalDateTime.now());
-        metaObject.setValue("updateUser",new Long(1));
+        metaObject.setValue("updateUser", new Long(123));
     }
 }
