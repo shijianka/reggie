ebe51d3d1cf3e6050ff80505425c1be246694a63
shijianka <shijianka@foxmail.com>
2022-03-08 11:58:35 +0800

    套餐信息分类查询  按照更新时间降序排列  （套餐分类未完善）

--- src/main/java/cn/shijianka/reggie/controller/SetmealController.java
+++ src/main/java/cn/shijianka/reggie/controller/SetmealController.java
@@ -2,15 +2,15 @@ package cn.shijianka.reggie.controller;
 
 import cn.shijianka.reggie.common.R;
 import cn.shijianka.reggie.dto.SetmealDto;
+import cn.shijianka.reggie.entity.Setmeal;
 import cn.shijianka.reggie.entity.SetmealDish;
 import cn.shijianka.reggie.service.SetmealDishService;
 import cn.shijianka.reggie.service.SetmealService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 套餐管理
@@ -28,5 +28,15 @@ public class SetmealController {
         log.info(setmealDto.toString());
         setmealService.addWithDish(setmealDto);
         return R.success("添加成功");
+    }
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize,String name){
+        Page<Setmeal> setmealPage=new Page<>(page,pageSize);
+        LambdaQueryWrapper<Setmeal> lqw=new LambdaQueryWrapper<>();
+        lqw.like(name!=null,Setmeal::getName,name)
+                .orderByDesc(Setmeal::getUpdateTime);
+        setmealService.page(setmealPage,lqw);
+        return R.success(setmealPage);
+
     }
 }
