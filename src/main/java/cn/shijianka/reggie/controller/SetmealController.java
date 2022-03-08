27fcff4b8f23dfa557169960e87d4af2039a0a77
shijianka <shijianka@foxmail.com>
2022-03-08 12:30:21 +0800

    完善套餐信息分页 --categoryName

--- src/main/java/cn/shijianka/reggie/controller/SetmealController.java
+++ src/main/java/cn/shijianka/reggie/controller/SetmealController.java
@@ -2,15 +2,21 @@ package cn.shijianka.reggie.controller;
 
 import cn.shijianka.reggie.common.R;
 import cn.shijianka.reggie.dto.SetmealDto;
+import cn.shijianka.reggie.entity.Category;
 import cn.shijianka.reggie.entity.Setmeal;
 import cn.shijianka.reggie.entity.SetmealDish;
+import cn.shijianka.reggie.service.CategoryService;
 import cn.shijianka.reggie.service.SetmealDishService;
 import cn.shijianka.reggie.service.SetmealService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 套餐管理
@@ -23,6 +29,8 @@ public class SetmealController {
     private SetmealService setmealService;
     @Autowired
     private SetmealDishService setmealDishService;
+    @Autowired
+    private CategoryService categoryService;
     @PostMapping
     public R<String> add(@RequestBody SetmealDto setmealDto){
         log.info(setmealDto.toString());
@@ -36,7 +44,33 @@ public class SetmealController {
         lqw.like(name!=null,Setmeal::getName,name)
                 .orderByDesc(Setmeal::getUpdateTime);
         setmealService.page(setmealPage,lqw);
-        return R.success(setmealPage);
+
+        //套餐分类需要将categoryId在页面上展示为分类名称，所以引入StemealDto，其中有categoryName属性
+        Page<SetmealDto> setmealDtoPage = new Page<>();
+        //用工具类BeanUtils完成两个对象之间属性的拷贝 , records,是Page对象的属性，表示页面需要展示的所有数据 所以在拷贝时忽略该属性（因为CategoryId还未处理）
+        BeanUtils.copyProperties(setmealPage,setmealDtoPage,"records");
+        //处理categoryid,获取含有categoryname的list集合
+        List<Setmeal> setmealRecords = setmealPage.getRecords();
+        List<SetmealDto> setmealDtoRecords = setmealRecords.stream().map(setmeal -> {
+            //获取categoryId
+            Long categoryId = setmeal.getCategoryId();
+            //获取category
+            Category category = categoryService.getById(categoryId);
+            //获取categoryName
+            String categoryName = category.getName();
+            //创建SetmealDto
+            SetmealDto setmealDto=new SetmealDto();
+            //拷贝Setmeal所有属性
+            BeanUtils.copyProperties(setmeal,setmealDto);
+            //将categoryName赋值给setmealDto
+            setmealDto.setCategoryName(categoryName);
+            //返回setmealDto
+            return setmealDto;
+        }).collect(Collectors.toList());
+        //将含有categoryname的list集合赋值给setmealDtoPage
+        setmealDtoPage.setRecords(setmealDtoRecords);
+        //返回setmealDtoPage
+        return R.success(setmealDtoPage);
 
     }
 }
