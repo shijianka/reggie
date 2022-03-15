3aee4d7abbef00bbacd9604205e95b06ecb78525
shijianka <shijianka@foxmail.com>
2022-03-16 00:49:33 +0800

    新增菜品管理的批量操作

--- src/main/java/cn/shijianka/reggie/controller/DishController.java
+++ src/main/java/cn/shijianka/reggie/controller/DishController.java
@@ -18,51 +18,51 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
 @RequestMapping("/dish")
-public class DishFlavorController {
+public class DishController {
     @Autowired
     private DishFlavorService dishFlavorService;
     @Autowired
     private DishService dishService;
     @Autowired
     private CategoryService categoryService;
+
     @PostMapping
-    public R<String> save(@RequestBody DishDto dishDto){
-            log.info(dishDto.toString());
+    public R<String> save(@RequestBody DishDto dishDto) {
+        log.info(dishDto.toString());
         dishService.saveWithFlavor(dishDto);
         return R.success("新增菜品成功");
     }
 
     /**
      * 分页查询
+     *
      * @return
      */
     @GetMapping("/page")
-    public R<Page> page(int page,int pageSize,String name){
+    public R<Page> page(int page, int pageSize, String name) {
         //构造分页构造器对象
-        Page<Dish> pDish = new Page<>(page,pageSize);
-        Page<DishDto> pDishDto = new Page<>(page,pageSize);
+        Page<Dish> pDish = new Page<>(page, pageSize);
+        Page<DishDto> pDishDto = new Page<>(page, pageSize);
 
         //条件构造器
-        LambdaQueryWrapper<Dish> lqw=new LambdaQueryWrapper<>();
+        LambdaQueryWrapper<Dish> lqw = new LambdaQueryWrapper<>();
         //添加过滤条件
-        lqw.like(name!=null,Dish::getName,name);
+        lqw.like(name != null, Dish::getName, name);
         //添加排序条件
         lqw.orderByDesc(Dish::getUpdateTime);
         //执行分页查询
-        dishService.page(pDish,lqw);
-        BeanUtils.copyProperties(pDish,pDishDto,"records");
+        dishService.page(pDish, lqw);
+        BeanUtils.copyProperties(pDish, pDishDto, "records");
         List<Dish> records = pDish.getRecords();
 
-        List<DishDto> list = records.stream().map(n->{
+        List<DishDto> list = records.stream().map(n -> {
             DishDto dishDto = new DishDto();
-            BeanUtils.copyProperties(n,dishDto);
+            BeanUtils.copyProperties(n, dishDto);
             Long categoryId = n.getCategoryId();//分类id
             //根据id查询分类对象
             Category category = categoryService.getById(categoryId);
@@ -80,51 +80,85 @@ public class DishFlavorController {
 
     /**
      * 数据回显
+     *
      * @param id
      * @return
      */
     @GetMapping("/{id}")
-    public R<DishDto> update1(@PathVariable Long id){
+    public R<DishDto> update1(@PathVariable Long id) {
         DishDto dishDto = dishService.getWithFlavor(id);
         return R.success(dishDto);
     }
 
     /**
      * 修改菜品
+     *
      * @param dishDto
      * @return
      */
     @PutMapping
     @Transactional
-    public R<String> update2(@RequestBody DishDto dishDto){
+    public R<String> update2(@RequestBody DishDto dishDto) {
         dishService.updateWithFlavor(dishDto);
         return R.success("修改成功");
     }
 
     @GetMapping("/list")
-    public R<List<DishDto>> list(Dish dish){
-      //构造条件构造器
-        LambdaQueryWrapper<Dish> lqw=new LambdaQueryWrapper<>();
+    public R<List<DishDto>> list(Dish dish) {
+        //构造条件构造器
+        LambdaQueryWrapper<Dish> lqw = new LambdaQueryWrapper<>();
         //查询分类为CategoryId的dish
-        lqw.eq(dish.getCategoryId()!=null,Dish::getCategoryId,dish.getCategoryId());
+        lqw.eq(dish.getCategoryId() != null, Dish::getCategoryId, dish.getCategoryId());
         //按照sort降序
         lqw.orderByDesc(Dish::getSort);
         //起售状态
-        lqw.eq(Dish::getStatus,1);
+        lqw.eq(Dish::getStatus, 1);
         //list集合
         List<Dish> list = dishService.list(lqw);
-     List<DishDto>  dishDtoList= list.stream().map(dish2->{
-         DishDto dishDto = new DishDto();
-         BeanUtils.copyProperties(dish2,dishDto);
-         LambdaQueryWrapper<DishFlavor> lqwDishFlavor=new LambdaQueryWrapper<>();
-         lqwDishFlavor.eq(DishFlavor::getDishId,dish2.getId());
-         List<DishFlavor> dishFlavorList=dishFlavorService.list(lqwDishFlavor);
+        List<DishDto> dishDtoList = list.stream().map(dish2 -> {
+            DishDto dishDto = new DishDto();
+            BeanUtils.copyProperties(dish2, dishDto);
+            LambdaQueryWrapper<DishFlavor> lqwDishFlavor = new LambdaQueryWrapper<>();
+            lqwDishFlavor.eq(DishFlavor::getDishId, dish2.getId());
+            List<DishFlavor> dishFlavorList = dishFlavorService.list(lqwDishFlavor);
 
-         dishDto.setFlavors(dishFlavorList);
-         return dishDto;
-     }).collect(Collectors.toList());
+            dishDto.setFlavors(dishFlavorList);
+            return dishDto;
+        }).collect(Collectors.toList());
 
 //        return R.success( dishService.list(lqw));
         return R.success(dishDtoList);
     }
+
+    /**
+     * 批量删除
+     * @param ids
+     * @return
+     */
+    @DeleteMapping
+    public R<String> delete(@RequestParam List<Long> ids) {
+        log.info("要删除的id==>{}",ids);
+        LambdaQueryWrapper<Dish> lqw=new LambdaQueryWrapper<>();
+        //要删除的那个菜品必须是停售，
+        lqw.eq(Dish::getStatus,0);
+        for (Long id : ids) {
+            //要删除的那个菜品必须是停售，
+            lqw.eq(Dish::getId,id);
+            dishService.remove(lqw);
+        }
+        return R.success("");
+    }
+
+    @PostMapping("/status/{status}")
+    public R<String> status(@PathVariable("status") Integer status,@RequestParam List<Long> ids){
+//        log.info("PathVaribale==> {} , ids ==> {}",status,ids);
+        List<Dish> dishes = dishService.listByIds(ids);
+        List<Dish> resultList = dishes.stream().map(dish -> {
+            dish.setStatus(status);
+            return dish;
+        }).collect(Collectors.toList());
+        dishService.saveOrUpdateBatch(resultList);
+        return R.success("");
+    }
+
 }
