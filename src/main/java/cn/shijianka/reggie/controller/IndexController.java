2a99161e8ac4f58c0a9bbaf834a54d7240ca2720
shijianka <shijianka@foxmail.com>
2022-03-17 09:59:35 +0800

    根据请求头分辨客户端完成前后台自动跳转

--- src/main/java/cn/shijianka/reggie/controller/IndexController.java
+++ src/main/java/cn/shijianka/reggie/controller/IndexController.java
@@ -1,26 +1,43 @@
 package cn.shijianka.reggie.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+@Slf4j
 @RestController
 @RequestMapping("/")
 public class IndexController {
     @Autowired
+    private HttpServletRequest httpServletRequest;
+    @Autowired
     private HttpServletResponse httpServletResponse;
+
+ /*   @GetMapping
+    public void index() {
+        httpServletResponse.setStatus(302);
+        httpServletResponse.setHeader("Location", "/backend/index.html");
+    }
+    @GetMapping("/{test}")
+    public void front() {
+        httpServletResponse.setStatus(302);
+        httpServletResponse.setHeader("Location", "/front/index.html");
+    }*/
 
     @GetMapping
     public void index() {
-        httpServletResponse.setStatus(302);
-        httpServletResponse.setHeader("Location","/backend/index.html");
-    }
-    @GetMapping("/{test}")
-    public void front(){
-        httpServletResponse.setStatus(302);
-        httpServletResponse.setHeader("Location","/front/index.html");
+        String header = httpServletRequest.getHeader("User-Agent");
+        log.info(header);
+        if(header.contains("windows")){
+            httpServletResponse.setStatus(302);
+            httpServletResponse.setHeader("Location", "/backend/index.html");
+        }else {
+            httpServletResponse.setStatus(302);
+            httpServletResponse.setHeader("Location", "/front/index.html");
+        }
     }
 }
